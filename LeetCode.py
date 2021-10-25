9bf35865c0565cfc0c3688161e51b049732cee50
Justice Martin <jujmart12@gmail.com>
2021-10-25 16:53:35 -0400

    Started problem 278

--- LeetCode.py
+++ LeetCode.py
@@ -1,32 +1,41 @@
 # Problem 704
-def search(nums, target):
-    if target < nums[0] or target > nums[-1]:
-        return -1
-    stack = [nums]
-    result = 0
-    while len(stack):
-        current = stack.pop()
-        if len(current) == 0:
-            return -1
-        mid_idx = len(current) // 2
-        mid_num = current[mid_idx]
-        if mid_num == target:
-            return result + mid_idx
-        elif mid_num > target:
-            stack.append(current[:mid_idx])
-        else:
-            result += mid_idx + 1
-            stack.append(current[mid_idx + 1:])
-    return result
+# def search(nums, target):
+#     if target < nums[0] or target > nums[-1]:
+#         return -1
+#     stack = [nums]
+#     result = 0
+#     while len(stack):
+#         current = stack.pop()
+#         if len(current) == 0:
+#             return -1
+#         mid_idx = len(current) // 2
+#         mid_num = current[mid_idx]
+#         if mid_num == target:
+#             return result + mid_idx
+#         elif mid_num > target:
+#             stack.append(current[:mid_idx])
+#         else:
+#             result += mid_idx + 1
+#             stack.append(current[mid_idx + 1:])
+#     return result
 
 
-print(search([-1, 0, 3, 5, 9, 12], 9))  # 4
-print(search([-1, 0, 3, 5, 9, 12], 2))  # -1
-print(search([-1, 0, 3, 5, 9, 12], 3))  # 2
-print(search([-1, 0, 3, 5, 9, 12], 0))  # 1
-print(search([-1, 0, 3, 5, 9, 12], -1))  # 0
-print(search([-1, 0, 3, 5, 9, 12], -2))  # -1
-print(search([-1, 0, 3, 5, 9, 12], 13))  # -1
-print(search([-1, 0, 3, 5, 9, 12], 12))  # 5
-print(search([-1, 0, 3, 5, 9, 12], 5))  # 3
-print(search([-1, 0, 3, 5, 9, 12], 7))  # -1
+# print(search([-1, 0, 3, 5, 9, 12], 9))  # 4
+# print(search([-1, 0, 3, 5, 9, 12], 2))  # -1
+# print(search([-1, 0, 3, 5, 9, 12], 3))  # 2
+# print(search([-1, 0, 3, 5, 9, 12], 0))  # 1
+# print(search([-1, 0, 3, 5, 9, 12], -1))  # 0
+# print(search([-1, 0, 3, 5, 9, 12], -2))  # -1
+# print(search([-1, 0, 3, 5, 9, 12], 13))  # -1
+# print(search([-1, 0, 3, 5, 9, 12], 12))  # 5
+# print(search([-1, 0, 3, 5, 9, 12], 5))  # 3
+# print(search([-1, 0, 3, 5, 9, 12], 7))  # -1
+
+
+# Problem 278
+def firstBadVersion(n):
+    pass
+
+
+print(firstBadVersion(5))
+print(firstBadVersion(1))
