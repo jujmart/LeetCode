511099d0290b3a15ab46aeed217b98ef29add085
Justice Martin <jujmart12@gmail.com>
2021-10-25 18:31:47 -0400

    Incorrect Problem 278

--- LeetCode.py
+++ LeetCode.py
@@ -34,8 +34,49 @@
 
 # Problem 278
 def firstBadVersion(n):
-    pass
+    if n == 1:
+        return 1
+    stack = [n // 2]
+    result = n
+    lastGoodVersion = 0
+    while len(stack) and lastGoodVersion < n:
+        current = stack.pop()
+        print(current, result)
+        if isBadVersion(current):
+            if current == lastGoodVersion + 1:
+                return current
+
+            if current == result + 1 or current == result:
+                return result
+
+            if current < result:
+                result = current
+                stack.append(current // 2)
+            else:
+                stack.append((current - result) // 2 + result)
+        else:
+            if lastGoodVersion < current:
+                lastGoodVersion = current
+            change = (n - current) // 2
+            if change == 0:
+                change = 1
+            stack.append(change + current)
+    return result
+
+
+def isBadVersion(n):
+    if n == 2:
+        return False
+    if n == 3:
+        return False
+    if n == 4:
+        return True
+    if n == 5:
+        return True
 
 
 print(firstBadVersion(5))
 print(firstBadVersion(1))
+print(firstBadVersion(4))
+print(firstBadVersion(3))
+print(firstBadVersion(2))
