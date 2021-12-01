a95645288359efd4e3d9da840a51e116b9e91fa4
Justice Martin <jujmart12@gmail.com>
2021-12-01 14:31:22 -0500

    Started Problem 40

--- LeetCode.py
+++ LeetCode.py
@@ -610,27 +610,36 @@
 
 
 # Problem 18
-def fourSum(nums, target):
-    nums.sort()
-    s = set()
-    for i in range(len(nums) - 3):
-        if i == 0 or nums[i] != nums[i - 1]:
-            for j in range(i + 1, len(nums)-2):
-                k = j + 1
-                l = len(nums) - 1
-                while k < l:
-                    total = nums[i] + nums[j] + nums[k] + nums[l]
-                    if total < target:
-                        k += 1
-                    elif total > target:
-                        l -= 1
-                    else:
-                        s.add((nums[i], nums[j], nums[k], nums[l]))
-                        k += 1
-                        l -= 1
+# def fourSum(nums, target):
+#     nums.sort()
+#     s = set()
+#     for i in range(len(nums) - 3):
+#         if i == 0 or nums[i] != nums[i - 1]:
+#             for j in range(i + 1, len(nums)-2):
+#                 k = j + 1
+#                 l = len(nums) - 1
+#                 while k < l:
+#                     total = nums[i] + nums[j] + nums[k] + nums[l]
+#                     if total < target:
+#                         k += 1
+#                     elif total > target:
+#                         l -= 1
+#                     else:
+#                         s.add((nums[i], nums[j], nums[k], nums[l]))
+#                         k += 1
+#                         l -= 1
 
-    return s
+#     return s
 
 
-print(fourSum([1, 0, -1, 0, -2, 2], 0))
-print(fourSum([2, 2, 2, 2, 2], 8))
+# print(fourSum([1, 0, -1, 0, -2, 2], 0))
+# print(fourSum([2, 2, 2, 2, 2], 8))
+
+# Problem 40
+def combinationSum(candidates, target):
+    pass
+
+
+print(combinationSum([10, 1, 2, 7, 6, 1, 5], 8))
+# [[1, 1, 6], [1, 2, 5], [1, 7], [2, 6]]
+print(combinationSum([2, 5, 2, 1, 2], 5))  # [[1, 2, 2], [5]]
